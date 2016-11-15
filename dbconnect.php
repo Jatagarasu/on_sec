b92cecf38afc3eea93c7b19c9cade2e3ccc23b07
Daniel <daniel.vogelskamp@gmail.com>
2016-11-15 15:13:49 +0100

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -5,4 +5,4 @@
  * Date: 15.11.2016
  * Time: 11:36
  */
-    $db = mysqli_connect("localhost", "onsec", "Onohu1Ofom70", "onsec") or DIE("Verbindungsfeheler:".mysqli_connect_error());
+    $db = mysqli_connect("localhost", "onsec", "Onohu1Ofom70", "onsec") or DIE("Verbindungsfehler:".mysqli_connect_error());
