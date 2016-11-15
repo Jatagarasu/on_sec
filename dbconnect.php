53f3f9d9736aff4d32713f7032e315a78fe2c8b5
Daniel Vogelskamp <daniel.vogelskamp@gmail.com>
2016-11-15 15:09:07 +0100

    dbconnect.php fuer lokale Datenbank angepasst

--- dbconnect.php
+++ dbconnect.php
@@ -5,7 +5,7 @@
  * Date: 15.11.2016
  * Time: 11:36
  */
-    $db = mysqli_connect("onsec.medien.hs-duesseldorf.de", "onsec", "Onohu1Ofom70", "onsec");
+    $db = mysqli_connect("localhost", "onsec", "Onohu1Ofom70", "onsec");
     if(!db) {
         exit("Verbindungsfehler:".mysqli_connect_error());
     }
