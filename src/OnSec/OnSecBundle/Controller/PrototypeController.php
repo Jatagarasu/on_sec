1e3020b68bdd501fc167f5defe2dda4b101e152b
david3141 <david.landry@veldrane.de>
2017-01-22 16:05:05 +0100

    Create Prototype Controller with views

---
+++ src/OnSec/OnSecBundle/Controller/PrototypeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OnSec\OnSecBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PrototypeController extends Controller {
+  public function indexAction() {
+    return $this->render('HSDOnSecBundle:Prototype:index.html.twig', array(
+      // ...
+    ));
+  }
+
+  public function dashboardAction() {
+    return $this->render('HSDOnSecBundle:Prototype:dashboard.html.twig', array(
+    ));
+  }
+
+  public function createLectureAction() {
+    return $this->render('HSDOnSecBundle:Prototype:createLecture.html.twig', array(
+    ));
+  }
+
+  public function createInstructionAction() {
+    return $this->render('HSDOnSecBundle:Prototype:createInstruction.html.twig', array(
+    ));
+  }
+}
