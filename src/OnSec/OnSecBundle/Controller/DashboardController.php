362bcaf50547d190c292b1fcfe80d0242c2a0530
Markus <maho6116>
2017-03-07 16:15:00 +0100

    Dashboard Update

--- src/OnSec/OnSecBundle/Controller/DashboardController.php
+++ src/OnSec/OnSecBundle/Controller/DashboardController.php
@@ -2,15 +2,41 @@
 
 namespace OnSec\OnSecBundle\Controller;
 
+use OnSec\OnSecBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
 {
     public function indexAction()
     {
+        $usercourses = $this->getCoursesByUserId(1);
+        //ToDo bitte dynamische UserId eintragen
+
+        $em = $this->getDoctrine()->getManager();
+
+        $courses = $em->getRepository('HSDOnSecBundle:Course')->findAll();
+
         return $this->render('HSDOnSecBundle:Dashboard:index.html.twig', array(
-            // ...
+            'courses' => $courses,
+            'usercourses' => $usercourses,
         ));
+    }
+
+    public function getCoursesByUserId($userId)
+    {
+        $usercourses = array();
+        $em = $this->getDoctrine()->getManager();
+
+        $courses = $em->getRepository('HSDOnSecBundle:Course')->findAll();
+
+        foreach ($courses as $course)
+        {
+            foreach ($course->getSubscriber() as $subscriber)
+            {
+                if($subscriber->getId() == $userId->getId())
+                    array_push($usercourses, $course);
+            }
+        }
     }
 
 }
